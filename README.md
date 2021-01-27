5164287794996538a7e39ec26e1db3a62268c907
Caspar Armster <caspar@armster.de>
2021-01-27 15:28:16 +0100

    [FIX] added README.md and made code more readable

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# the_things_stack_v3_tabs_decoder
+# The Things Stack V3 tabs decoder
 Decoder in javascript for the family of tabs sensors for the things stack v3
+
+The differnt tabs sensors supported by this javascript decoder:
+- Door & Window Sensor ([Link to BROWAN Productpage](https://www.browan.com/product/door-window-sensor/detail))
+- Motion Sensor (PIR) ([Link to BROWAN Productpage](https://www.browan.com/product/motion-sensor-pir/detail))
+- Healthy Home Sensor IAQ ([Link to BROWAN Productpage](https://www.browan.com/product/healthy-home-sensor-iaq/detail))
+- Ambient Light Sensor ([Link to BROWAN Productpage](https://www.browan.com/product/ambient-light-sensor/detail))
+- Sound Level Sensor ([Link to BROWAN Productpage](https://www.browan.com/product/sound-level-sensor/detail))
+- Water Leak Sensor ([Link to BROWAN Productpage](https://www.browan.com/product/water-leak-sensor/detail))
+- Object Locator ([Link to BROWAN Productpage](https://www.browan.com/product/object-locator/detail))
+
+This decoder is slightly different than the decoder used for the old V2 stack!
+
+To integrate the decoder navigate please to [https://eu1.cloud.thethings.network/console/](https://eu1.cloud.thethings.network/console/) and click on "Go to applications" and choose the application for the tabs sensors (or create one). On the left side you find "Payload formatters" and then choose "Uplink". Choose the Formatter type "Javascript", copy and paste the hole code and overwrite the code in die window and click "Save changes". From now on the payload from the tabs sensors gets decoded into readable JSON output.
+
+You can now integrate all the differnt tabs sensors (listed above) and the decoder automaticly decodes the right data.
+
+This decoder is licensed by the AGPL-3.0 License, please have a look at the LICENSE file.
--- decodeUplink.js
+++ decodeUplink.js
@@ -1,14 +1,16 @@
 /* Decoder in javascript for the family of tabs sensors from BROWAN for The Things Stack V3
  *
+ * GNU Affero General Public License v3.0 - look into the LICENSE file please
+ *
  * Created by Caspar Armster (dasdigidings e.V. / The Things Network Rhein-Sieg) - www.dasdigidings.de
  * This function is based on the work from Cameron Sharp at Sensational Systems - cameron@sensational.systems
  */
 
 function decodeUplink(input) {
-
+    // create the object to collect the data for returning the decoded payload
     var data = {
         "bytes": input.bytes, // original payload
-        "port" : input.fPort  // port
+        "port" : input.fPort  // lorawan port
     };
 
     // Every device transmits the battery status and the temperature
@@ -17,6 +19,7 @@ function decodeUplink(input) {
     battery = (25 + battery) / 10;
     capacity = input.bytes[1] >> 4;
     capacity = (capacity / 15) * 100;
+
     // Temperature measurement
     temperature = input.bytes[2] & 0x7f;
     temperature = temperature - 32;
@@ -25,6 +28,7 @@ function decodeUplink(input) {
     data.capacity = capacity;
     data.temperature = temperature;
 
+    // depending on the lorawan port we know which tabs sensor is delivering the data
     if (input.fPort === 100) { // Door & Window Sensor
         // Time measurement
         openingStatusTime = (input.bytes[4] << 8) | input.bytes[3];
@@ -89,7 +93,7 @@ function decodeUplink(input) {
             humidityError = false;
         }
 
-        // Environment temp measurement
+        // Environment temperature measurement
         temperatureEnvironment = input.bytes[10] & 0x7f;
         temperatureEnvironment = temperatureEnvironment - 32;
 
@@ -101,9 +105,9 @@ function decodeUplink(input) {
         data.humidity = humidity;
         data.humidityError = humidityError;
         data.temperatureEnvironment = temperatureEnvironment;
-    } else if (input.fPort === 104) { // Sound Level Sensor
+    } else if (input.fPort === 104) { // Ambient Light Sensor
         // Lux measurement
-        lux = (input.bytes[5] << 16) | (input.bytes[4] << 8) | input.bytes[3];
+        lux = ((input.bytes[5] << 16) | (input.bytes[4] << 8)) | input.bytes[3];
         lux = lux / 100;
 
         data.lux = lux;
@@ -119,7 +123,7 @@ function decodeUplink(input) {
         data.soundLevel = soundLevel;
         data.soundLevelError = soundLevelError;
     } else if (input.fPort === 106) { // Water Leak Sensor
-        // Environment temp measurement
+        // Environment temperature measurement
         temperatureEnvironment = input.bytes[4] & 0x7f;
         temperatureEnvironment = temperatureEnvironment - 32;
 
@@ -141,20 +145,22 @@ function decodeUplink(input) {
         } else {
             positionGnssFix = false;
         }
-      
+
+        // Accuracy Measurement
         positionAccuracy = input.bytes[10] >> 5;
         positionAccuracy = Math.pow(2, parseInt(acc) + 2);
-      
+
         // Mask off end of accuracy byte, so longitude doesn't get affected
         input.bytes[10] &= 0x1f;
-    
+
         if ((input.bytes[10] & (1 << 4)) !== 0) {
           input.bytes[10] |= 0xe0;
         }
-      
+
         // Mask off end of latitude byte, RFU
-        input.bytes[6] &= 0x0f; 
-      
+        input.bytes[6] &= 0x0f;
+
+        // Latitude and Longitude Measurement
         positionLatitude = ((input.bytes[6] << 24 | input.bytes[5] << 16) | input.bytes[4] << 8 ) | input.bytes[3];
         positionLongitude = ((input.bytes[10] << 24 | input.bytes[9] << 16) | input.bytes[8] << 8 ) | input.bytes[7];
         positionLatitude = positionLatitude / 1000000;
