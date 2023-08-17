8b0abec0d7b5284306082faaf3472291e2c1b122
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2023-08-17 10:58:21 +0200

    Update decodeUplink.js
    Removed 3 Lines for Button Pressed, Moving and GNSS 0k i didnt need it and it drops Failures every time i want to use it!
    Removed the "position" in data fields to let TTN understand the Location and make it usable with ttnmapper

--- decodeUplink.js
+++ decodeUplink.js
@@ -4,6 +4,7 @@
  *
  * Created by Caspar Armster (dasdigidings e.V. / The Things Network Rhein-Sieg) - www.dasdigidings.de
  * This function is based on the work from Cameron Sharp at Sensational Systems - cameron@sensational.systems
+ * This Edit ist based on the PR from https://github.com/mfalkvidd in https://github.com/dasdigidings/the_things_stack_v3_tabs_decoder
  */
 
 function decodeUplink(input) {
@@ -12,24 +13,36 @@ function decodeUplink(input) {
         "bytes": input.bytes, // original payload
         "port" : input.fPort  // lorawan port
     };
+    var batteryStatus = input.bytes[1];
 
     // Every device transmits the battery status and the temperature
     // Battery measurement
-    battery = input.bytes[1] & 0x0f;
-    battery = (25 + battery) / 10;
-    capacity = input.bytes[1] >> 4;
-    capacity = (capacity / 15) * 100;
+    switch(batteryStatus){
+        case 0:
+            // Device is charging or line powered.
+            data.charging = true;
+            data.batteryFault = false;
+            break;
+        case 255:
+            // Device could not measure battery — possible Fault
+            data.batteryFault = true;
+            break;
+        default:
+            data.charging = false;
+            data.batteryFault = false;
+            battery = input.bytes[1] & 0x0f;
+            battery = (25 + battery) / 10;
+            data.battery = battery;
+            capacity = input.bytes[1] >> 4;
+            capacity = (capacity / 15) * 100;
+            data.capacity = capacity;
+    }
 
     // Temperature measurement
     temperature = input.bytes[2] & 0x7f;
     temperature = temperature - 32;
-
-    data.battery = battery;
-    data.capacity = capacity;
     data.temperature = temperature;
-
-    // depending on the lorawan port we know which tabs sensor is delivering the data
-    if (input.fPort === 100) { // Door & Window Sensor
+if (input.fPort === 100) { // Door & Window Sensor
         // Time measurement
         openingStatusTime = (input.bytes[4] << 8) | input.bytes[3];
 
@@ -47,117 +60,14 @@ function decodeUplink(input) {
         data.openingStatusTime = openingStatusTime;
         data.openingStatusCount = openingStatusCount;
         data.openingStatusOpen = openingStatusOpen;
-    } else if (input.fPort === 102) { // Motion Sensor (PIR)
-        // Time measurement
-        roomStatusTime = (input.bytes[4] << 8) | input.bytes[3];
-    
-        // Count measurement
-        roomStatusCount = ((input.bytes[7] << 16) | (input.bytes[6] << 8)) | input.bytes[5];
-    
-        // Status measurement
-        roomStatus = input.bytes[0] & 0x1;
-        if (roomStatus === 1) {
-            roomStatusOccupied = true;
-        } else {
-            roomStatusOccupied = false;
-        }
-    
-        data.roomStatusTime = roomStatusTime;
-        data.roomStatusCount = roomStatusCount;
-        data.roomStatusOccupied = roomStatusOccupied;
-    } else if (input.fPort === 103) { // Healthy Home Sensor IAQ & Temperature & Humidity Sensor
-        if (input.bytes.length > 8) { // Healthy Home Sensor IAQ
-            // VOC Measurement
-            voc = (input.bytes[7] << 8) | input.bytes[6];
-            if (voc === 65535) {
-                vocError = true;
-            } else {
-                vocError = false;
-            }
-
-            // CO2 Measurement
-            co2 = (input.bytes[5] << 8) | input.bytes[4];
-            if (co2 === 65535) {
-                co2Error = true;
-            } else {
-                co2Error = false;
-            }
-
-            // IAQ Measurement
-            iaq = (input.bytes[9] << 9) | input.bytes[8];
-
-            // Environment temperature measurement
-            temperatureEnvironment = input.bytes[10] & 0x7f;
-            temperatureEnvironment = temperatureEnvironment - 32;
-            
-            data.voc = voc;
-            data.vocError = vocError;
-            data.co2 = co2;
-            data.co2Error = co2Error;
-            data.iaq = iaq;
-            data.temperatureEnvironment = temperatureEnvironment;
-        }
-
-        // Humidity Measurement
-        humidity = input.bytes[3] &= 0x7f;
-        if (humidity === 127) {
-            humidityError = true;
-        } else {
-            humidityError = false;
-        }
-
-        data.humidity = humidity;
-        data.humidityError = humidityError;
-    } else if (input.fPort === 104) { // Ambient Light Sensor
-        // Lux measurement
-        lux = ((input.bytes[5] << 16) | (input.bytes[4] << 8)) | input.bytes[3];
-        lux = lux / 100;
-
-        data.lux = lux;
-    } else if (input.fPort === 105) { // Sound Level Sensor
-        // Sound Level measurement
-        soundLevel = input.bytes[3] & 0xff;
-        if (soundLevel === 255) {
-            soundLevelError = true;
-        } else {
-            soundLevelError = false;
-        }
-
-        data.soundLevel = soundLevel;
-        data.soundLevelError = soundLevelError;
-    } else if (input.fPort === 106) { // Water Leak Sensor
-        // water leakage status bit
-        waterLeakageBit = input.bytes[0] & 0x01;
-        if (waterLeakageBit === 1) {
-            waterLeakage = true;
-        } else {
-            waterLeakage = false;
-        }
-        
-        // Environment temperature measurement
-        temperatureEnvironment = input.bytes[4] & 0x7f;
-        temperatureEnvironment = temperatureEnvironment - 32;
-
-        // Humidity Measurement
-        humidity = input.bytes[3] &= 0x7f;
-        if (humidity === 127) {
-            humidityError = true;
-        } else {
-            humidityError = false;
-        }
-
-        data.waterLeakage = waterLeakage;
-        data.temperatureEnvironment = temperatureEnvironment;
-        data.humidity = humidity;
-        data.humidityError = humidityError;
-    } else if (input.fPort === 136) { // Object Locator
+    }
+     else if (input.fPort === 136) { // Object Locator
         // GNSS Fix?
         if ((input.bytes[0] & 0x8) === 0) {
             positionGnssFix = true;
         } else {
             positionGnssFix = false;
         }
-
         // Accuracy Measurement
         positionAccuracy = input.bytes[10] >> 5;
         positionAccuracy = Math.pow(2, parseInt(positionAccuracy) + 2);
@@ -178,10 +88,11 @@ function decodeUplink(input) {
         positionLatitude = positionLatitude / 1000000;
         positionLongitude = positionLongitude / 1000000;
 
-        data.positionGnssFix = positionGnssFix;
-        data.positionLatitude = positionLatitude;
-        data.positionLongitude = positionLongitude;
-        data.positionAccuracy = positionAccuracy;
+        // removed "position" after "data." to let TTN understand the Location and make it usable for TTN-Mapper.org
+        data.gnssfix = positionGnssFix;
+        data.latitude = positionLatitude;
+        data.longitude = positionLongitude;
+        data.accuracy = positionAccuracy;
     }
 
     return {
